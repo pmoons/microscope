17e6b8e3f1b4fb5ec63db668b68aad8e456b378e
Peter Mooney <peter.mooney90@gmail.com>
2015-07-13 15:46:36 -0400

    Demonstrate the order that posts appear using a sleep.

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -17,8 +17,9 @@ Template.postSubmit.events({
 			if (result.postExists)
 				alert('This link has already been posted');
 
-			Router.go('postPage', {_id:
-				result._id});
+			
 		});
+
+		Router.go('postsList');
 	}
 });
